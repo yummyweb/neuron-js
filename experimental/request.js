ffc07717992da86feb39011b99531369f66627eb
antriksh123 <antariksha.verma@pathways.in>
2020-11-09 13:15:29 +0530

    Deleted experimental folder

--- experimental/request.js
+++
@@ -1,9 +0,0 @@
-import url from 'url'
-
-function request(req) {
-	const parsedUrl = url.parse(`${req.headers.host}${req.url}`, true);
-	const keys = Object.keys(parsedUrl);
-	keys.forEach(key => (req[key] = parsedUrl[key]));
-}
-
-export default request
